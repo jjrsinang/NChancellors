f26d0a80c3f3b8f8a5ff38162693ff58aa44bfa4
John Joseph R. Sinang <jjrsinang@gmail.com>
2015-11-09 23:32:06 +0800

    N Chancellors with initial solutions
    working(?) n chancellors with initial solutions. still has no file
    reading

--- NChancellors/main.c
+++ NChancellors/main.c
@@ -8,30 +8,32 @@
 //
 
 #include <stdio.h>
-#define N 8
+#define N 5
 #define TRUE 1
 #define FALSE 0
 
+int Nf = 0;
+int initial[N+1];
+
 void print_solution(int n,int x[]) {
-    char c[N+1][N+1];
     int i,j;
-    
+    int solution[N+1][N+1];
     // pre-fill with dashes
     for(i=1;i<=n;i++) {
         for(j=1; j<=n; j++) {
-            c[i][j]='-';
+            solution[i][j]='-';
         }
     }
     
     // fill solutions
     for(i=1;i<=n;i++) {
-        c[i][x[i]]='Q';
+        solution[i][x[i]]='C';
     }
     
     // print table
     for( i=1;i<=n;i++) {
         for(j=1;j<=n;j++) {
-            printf("\t%c",c[i][j]);
+            printf("\t%c",solution[i][j]);
         }
         printf("\n");
     }
@@ -47,26 +49,47 @@ int place(int x[],int k) {
         }
 
         if (i-x[i]==k-x[k]) { // if same back-diagonal
-            return FALSE;
+            //return FALSE;
         }
 
         if (i+x[i]==k+x[k]) { // if same forward-diagonal
+            //return FALSE;
+        }
+        // knight moves
+        if (i==k-1 && x[i]==x[k]-2) { // col-2, row-1
             return FALSE;
         }
-
-        // knight moves - (col+-2 && row+-1) || (col+-1 && row+-2)
-        if(i-x[i]==k-x[k]) {
+        if (i==k+1 && x[i]==x[k]-2) { // col-2, row+1
+            return FALSE;
+        }
+        if (i==k-1 && x[i]==x[k]+2) { // col+2, row-1
+            return FALSE;
+        }
+        if (i==k+1 && x[i]==x[k]+2) { // col+2, row+1
+            return FALSE;
+        }
+        if (i==k-2 && x[i]==x[k]-1) { // col-1, row-2
+            return FALSE;
+        }
+        if (i==k+2 && x[i]==x[k]-1) { // col-1, row+2
+            return FALSE;
+        }
+        if (i==k-2 && x[i]==x[k]+1) { // col+1, row-2
+            return FALSE;
+        }
+        if (i==k+2 && x[i]==x[k]+1) { // col+1, row+2
             return FALSE;
         }
     }
+
     return TRUE;
 }
 
-void nqueens(int n) {
+void nchancellors(int n) {
     int x[N+1]; // queen positions: row=index, column=value
     int count=0;
     int k=1;
-    
+
     x[k]=0;
     
     while(k!=0) {
@@ -76,9 +99,17 @@ void nqueens(int n) {
         }
         if(x[k]<=n) {
             if(k==n) { // n queens have been placed
-                count++;
-                printf("\n\tSolution %d  is : \n\n\n",count);
-                print_solution(n,x);
+                int i, hasInitialSolution = TRUE;
+                for (i=1; i<=N; i++) {
+                    if (initial[i]>0 && initial[i]!=x[i]) {
+                        hasInitialSolution = FALSE;
+                    }
+                }
+                if (hasInitialSolution) {
+                    count++;
+                    printf("\n\tSolution %d  is : \n\n\n",count);
+                    print_solution(n,x);
+                }
             } else { // next queen
                 k++;
                 x[k]=0;
@@ -87,15 +118,21 @@ void nqueens(int n) {
             k--;
         }
     }
+
+    if (count==0) {
+        printf("\n\tPuzzle has no solutions\n");
+    }
     return;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    //int n;
-    //printf("\n Enter the no. of Queens : ");
-    //scanf("%d",&n);
+    int i;
+    for (i=1; i<=N; i++) {
+        initial[i] = 0;
+    }
+    initial[3] = 3;
+
     printf("\n\n\t\t\t USING %d QUEEN'S STRATEGY \n\n",N);
-    nqueens(N);
+    nchancellors(N);
     return 0;
 }
